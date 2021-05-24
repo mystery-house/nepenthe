a098a612515703ce9c42a008fee2a98f86d80ff5
Andy Chase <andychase@gmail.com>
2021-05-23 22:58:10 -0400

    #8: input file argument

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
+const { ArgumentParser } = require('argparse')
 const yamlFront = require('yaml-front-matter');
 const lilypond = require('../lilypond.js')
+const { exit } = require('process');
 
+const parser = new ArgumentParser({
+    description: 'PitcherPlant - Lilypond pre-processor'
+});
+parser.add_argument({dest: 'inputFile', help: 'the path to the file to be processed'})
 
-fs.readFile('./test.lyp', 'utf8', (err, data) => {
+let args = parser.parse_args()
+
+if(!fs.existsSync(args.inputFile)) {
+    process.stderr.write(`\nNo such file \`${args.inputFile}\`\n\n`)
+    exit(1)
+}
+
+fs.readFile(args.inputFile, 'utf8', (err, data) => {
     if(err) {
         console.error(err)
         return
