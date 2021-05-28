1379b47cad439abc2281593a300396cd67d4ad71
Andy Chase <andychase@gmail.com>
2021-05-28 11:45:50 -0400

    Misc. refactoring, implementation of handlebars "{{part 'partName'}}" tags

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 const { ArgumentParser } = require('argparse')
 const yamlFront = require('yaml-front-matter');
-const lilypond = require('../lilypond.js')
+const { renderLilypond } = require('../lib/lilypond.js')
 const { exit } = require('process');
 const { version } = require('../package.json')
 const { homepage } = require('../package.json')
+const hbs = require('handlebars')
+const { part } = require('../lib/handlebars.js')
+//const util = require('util')
+
+
+module.exports = {parseInputFile, main}
 
 
 function parseInputFile(file) {
@@ -43,8 +49,45 @@ function main() {
     // TODO error handling if file can't be read
     
     let data = parseInputFile(inputFile)
-    
-    let outputData = lilypond.render(data)
+
+    // After processing YAML front-matter, the actual input is left in the __content
+    // property of the `data` object. The next step is to preprocess __content on 
+    // its own to extract any parts that may have been explicitly declared.
+    // There is probably a cleverer/built-in way to do this, but it's a start:
+
+    hbs.registerHelper('part', part(data))
+    hbs.registerHelper('whichStave', (stave) => {return `staves_${stave}`})
+    let input = hbs.compile(data.__content)()
+
+    // If no parts were explicitly defined then create a single default part
+    // using the entire input
+    if(data['parts'] == undefined) {
+        data['parts'] = {'part1': input}
+        data['input'] = ''
+    // If there were some parts in the input, then pass along whatever's left
+    // in the processed input.
+    } else {
+        data['input'] = input
+    }
+
+    // console.dir(data)
+
+    // If no layout instructions were explicitly defined then create a simple
+    // default with a staff for each part.
+    if(data['layout'] == undefined) {
+        data['layout'] = []
+        for (let [part, val] of Object.entries(data['parts'])) {
+            let layout = {}
+            layout[part] = ["default"]
+            data['layout'].push(layout)
+        }
+    }
+
+    // Remove __content now that we're done with it.
+    delete data['__content']
+
+    // Now Render the internal templates
+    let outputData = renderLilypond(data)
     
     if(args.outputFile == '-') {
         process.stdout.write(outputData)
@@ -57,5 +100,3 @@ function main() {
 if(require.main === module) {
     main()
 }
-
-module.exports = {parseInputFile, main}
