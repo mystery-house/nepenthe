90ad872d4a43416bfa7f01cfce9693f294f8f2ef
Andy Chase <andychase@gmail.com>
2021-08-07 08:20:33 -0400

    Code linting

--- src/cli.ts
+++ src/cli.ts
@@ -1,45 +1,53 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import { parseInputFile } from "./nepenthe";
-import { exit } from 'process';
+import { exit } from "process";
 import { readFileSync } from "fs";
-const yamlFront = require('yaml-front-matter');
-import {modeHelper, scoreHelper, staffHelper } from "./handlebars"
-import { hasSubscribers } from 'diagnostic_channel';
+const yamlFront = require("yaml-front-matter");
+import { modeHelper, scoreHelper, staffHelper } from "./handlebars";
+import { hasSubscribers } from "diagnostic_channel";
 import hbs from "handlebars";
 
-module.exports = {main}
+module.exports = { main };
 
 /**
  * The main function for the `nepenthe` command.
  */
 function main() {
-
-    let data = parseInputFile('Herbert Ellis - Firefly Jig.nep')  // TODO parse input args
+    let data = parseInputFile("Herbert Ellis - Firefly Jig.nep"); // TODO parse input args
     // let input = hbs.compile(data['input'])
-    hbs.registerHelper('mode', modeHelper)
-    hbs.registerHelper('score', scoreHelper)
-    hbs.registerHelper('staff', staffHelper)
+    hbs.registerHelper("mode", modeHelper);
+    hbs.registerHelper("score", scoreHelper);
+    hbs.registerHelper("staff", staffHelper);
 
-    hbs.registerPartial('partPartial', fs.readFileSync('./src/templates/partials/part.hbs', 'utf-8'))
-    hbs.registerPartial('scorePartial', fs.readFileSync('./src/templates/partials/score.hbs', 'utf-8'))
-    hbs.registerPartial('staffPartial', fs.readFileSync('./src/templates/partials/staff.hbs', 'utf-8'))
+    hbs.registerPartial(
+        "partPartial",
+        fs.readFileSync("./src/templates/partials/part.hbs", "utf-8")
+    );
+    hbs.registerPartial(
+        "scorePartial",
+        fs.readFileSync("./src/templates/partials/score.hbs", "utf-8")
+    );
+    hbs.registerPartial(
+        "staffPartial",
+        fs.readFileSync("./src/templates/partials/staff.hbs", "utf-8")
+    );
 
     // Compile and render the template data from the Nepenthe document
-    let tpl = hbs.compile(data.input)
-    data.content = tpl(data)
+    let tpl = hbs.compile(data.input);
+    data.content = tpl(data);
 
     // Compile and render the base template (passing the rendered Nepenthe doc
     // body from the previous step.)
-    let base = hbs.compile(fs.readFileSync('./src/templates/base.hbs', 'utf-8'))
-    let final =     base(data)
-    console.log(final)
+    let base = hbs.compile(fs.readFileSync("./src/templates/base.hbs", "utf-8"));
+    let final = base(data);
+    console.log(final);
 }
 
- // If index.js has been invoked directly, run the main() function:
-if(require.main === module) {
-    main()
-    exit(0)
+// If index.js has been invoked directly, run the main() function:
+if (require.main === module) {
+    main();
+    exit(0);
 }
